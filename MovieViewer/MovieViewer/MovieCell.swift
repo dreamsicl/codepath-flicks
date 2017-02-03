fa914d850a392c790d3598bb92045aa4bc025759
dreamsicl <vphong@ucsd.edu>
2017-02-02 19:00:28 -0800

    qs2 revision

---
+++ MovieViewer/MovieViewer/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Vanna Phong on 2/2/17.
+//  Copyright © 2017 Vanna Phong. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
