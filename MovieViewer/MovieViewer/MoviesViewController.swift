5757aae97b41397bbc17b67adcc4bccdc9f74e5a
dreamsicl <vphong@ucsd.edu>
2017-02-06 14:39:44 -0800

    detail view controller

--- MovieViewer/MovieViewer/MoviesViewController.swift
+++ MovieViewer/MovieViewer/MoviesViewController.swift
@@ -257,5 +257,20 @@ class MoviesViewController: UIViewController,/* UITableViewDataSource, UITableVi
         return cell
 
     }
-
+    
+    // MARK: - Navigation
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)
+        let movie = filteredMovies?[(indexPath?.row)!]
+        
+        let detailViewController = segue.destination as! DetailViewController
+        detailViewController.movie = movie
+        
+        
+    }
 }
